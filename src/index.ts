08afac2c2496ab387588e3823664504bc4f70f74
Dean Ford <66484815+dem1fo@users.noreply.github.com>
2024-11-27 14:37:41 +0300

    export all required elements

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 export { loadConfig } from './config/loadConfig.js';
+export { makeConfigStore, ConfigStore } from './config/configStore.js';
 export {
   generateHashesForDocument,
   preprocessFile,
   processFile,
   validateDocument,
 } from './processing/index.js';
-export { CidDocument } from './document.js';
+export { CidDocument, SUPPORTED_FILE_TYPES } from './document.js';
 export { makeValidationResultDocument } from './validation/index.js';
-export { keepOutputColumns } from './processing/mapping.js';
+export { keepOutputColumns } from './processing/mapping.js'; 
+export type { Validation } from './validation/Validation.js';
+
+export { joinFieldsForHash, cleanValueList, extractAlgoColumnsFromObject } from './hashing/utils.js';
+export { BaseHasher } from './hashing/base.js';
+export type { makeHasherFunction } from './hashing/base.js';
+export type { Config } from './config/Config.js';
